121b38a00ea6230470b9f219fecf07ed1eb36eaa
MBing <bing.martin.photography@gmail.com>
2013-03-26 10:27:15 +0100

    removing some comments

--- mb/content.php
+++ mb/content.php
@@ -1,9 +1,3 @@
-<?php
-/**
- * @package MB
- */
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'mb' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
